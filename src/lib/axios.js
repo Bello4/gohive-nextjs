e5eb2ed12a80025d602f43e69b6efe12ea3741c2
Bello4 <you@example.com>
2025-12-02 08:56:39 +0100

    updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://api.hivego.ng",
   headers: {
     "X-Requested-With": "XMLHttpRequest",
   },
